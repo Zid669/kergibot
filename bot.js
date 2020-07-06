f45c3f02c1548fcbbe5e0092c5c46387f20e8705
Zid669 <65919578+Zid669@users.noreply.github.com>
2020-07-06 20:07:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on("ready", () => {
     client.user.setActivity({name: "Kergi :p", type: "STREAMING", url: "https://www.twitch.tv/kergitv"});
 });
 
-client.on("messageReactionAdd", async (reaction, user) => {
+client.on("messageReactionRemove", async (reaction, user) => {
     // If a message gains a reaction and it is uncached, fetch and cache the message.
     // You should account for any errors while fetching, it could return API errors if the resource is missing.
     if (reaction.message.partial) await reaction.message.fetch(); // Partial messages do not contain any content so skip them.
